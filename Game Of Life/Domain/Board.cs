4a07daccd681f035b466d903eb1d25b5f8b46105
AlexMtGit <alex.marexal@gmail.com>
2021-10-12 12:55:47 +0200

    Adding Domain classes
    In this commit the project has been created.
    
    The 2 main classes (included in Domain folder) Cell.cs and Board.cs has been developed.

---
+++ Game Of Life/Domain/Board.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Of_Life.Domain
+{
+    public class Board
+    {
+        
+        private Cell[,] _cells; // 2D array that represents cells grid
+        private int _cellSize;  // Integer that represents cell size (in pixels)
+
+        // Getters
+        public Cell[,] Cells { get => _cells; }
+        public int CellSize { get => _cellSize; }
+        public int Columns { get => _cells.GetLength(0); }  // Grid's columns number
+        public int Rows { get => _cells.GetLength(1); }     // Grid's rows number
+        public int Width { get => Columns * CellSize; }     // Grid's width (in pixels)
+        public int Height { get => Rows * CellSize; }       // Grid's height (in pixels)
+
+        // Board builder.
+        // The 4th parameter indicates if the edges are connected as if the board is folded.
+        //      If it is false, the board is not folded and the edges are not taken into account.
+        public Board(int width, int height, int cellSize, bool fold = true)
+        {
+            _cellSize = cellSize;
+
+            _cells = new Cell[width / cellSize, height / cellSize];
+            for (int x = 0; x < Columns; x++)
+                for (int y = 0; y < Rows; y++)
+                    Cells[x, y] = new Cell();
+
+            InitializeNeighbours(fold);
+        }
+
+        // This method is responsible for properly establishing the neighbours of each cell
+        private void InitializeNeighbours(bool fold)
+        {
+            for (int x = 0; x < Columns; x++)
+            {
+                for (int y = 0; y < Rows; y++)
+                {
+                    // Determining whether the cell is an edge or not
+                    bool lE = (x == 0);
+                    bool rE = (x == Columns - 1);
+                    bool tE = (y == 0);
+                    bool bE = (y == Rows - 1);
+                    bool isEdge = lE | rE | tE | bE;
+
+                    if ((fold == false) && isEdge)
+                        continue;
+
+                    int xL = lE ? Columns - 1 : x - 1;  // Left neighbour position
+                    int xR = rE ? 0 : x + 1;            // Right neighbour position
+                    int yT = tE ? Rows - 1 : y - 1;     // Top neighbour position
+                    int yB = bE ? 0 : y + 1;            // Bottom neighbour position
+
+                    // Adding the neightbour to the cell
+                    Cells[x, y].neighbours.Add(Cells[xL, yT]);
+                    Cells[x, y].neighbours.Add(Cells[x, yT]);
+                    Cells[x, y].neighbours.Add(Cells[xR, yT]);
+                    Cells[x, y].neighbours.Add(Cells[xL, y]);
+                    Cells[x, y].neighbours.Add(Cells[xR, y]);
+                    Cells[x, y].neighbours.Add(Cells[xL, yB]);
+                    Cells[x, y].neighbours.Add(Cells[x, yB]);
+                    Cells[x, y].neighbours.Add(Cells[xR, yB]);
+                }
+            }
+        }
+
+        // This method can be used to randomly initialise the cells of the board.
+        // The number of live cells depends on the liveStep threshold.
+        public void RandomlyDistribute(double liveStep)
+        {
+            Random random = new Random();
+
+            foreach (var cell in Cells)
+                cell.Alive = random.NextDouble() < liveStep;
+        }
+
+        // Updates the live status of all cells on the board
+        public void NextStep()
+        {
+            foreach (var cell in Cells)
+                cell.CalculateNextLiveStatus();
+            foreach (var cell in Cells)
+                cell.NextStep();
+        }
+
+    }
+}
---
+++ Game Of Life/Domain/Cell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Game_Of_Life.Domain
+{
+	public class Cell
+	{
+
+		// This boolean field indicates if the cell is alive or not
+		private bool _alive;
+		// This boolean field indicates if the cell will be alive or not in the next Step
+		private bool _aliveNextStep;
+		// This list contains the 8 neighbours of the current cell
+		public readonly List<Cell> neighbours = new List<Cell>();
+
+		//Getters and setters
+        public bool Alive { get => _alive; set => _alive = value; }
+        public bool AliveNextStep { get => _aliveNextStep; set => _aliveNextStep = value; }
+
+        // Calculates if the cell will be alive or not in the next step
+        public void CalculateNextLiveStatus()
+        {
+			// Any live cell with two or three live neighbours survives.
+			// Any dead cell with three live neighbours becomes a live cell.
+			// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+			int neighboursAlive = neighbours.Where(n => n.Alive).Count();
+
+			if (Alive)
+				AliveNextStep = neighboursAlive == 2 || neighboursAlive == 3;
+			else
+				AliveNextStep = neighboursAlive == 3;
+		}
+
+		// Updates live status of the cell
+		public void NextStep()
+        {
+			Alive = AliveNextStep;
+        }
+	}
+}
+
